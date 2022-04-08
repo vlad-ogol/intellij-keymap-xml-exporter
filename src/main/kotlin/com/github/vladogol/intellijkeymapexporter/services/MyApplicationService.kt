fdc38118fefe9e47c0d58f60fbb6d38b988d7556
GitHub Action <action@github.com>
2022-04-08 18:19:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vladogol/intellijkeymapexporter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vladogol.intellijkeymapexporter.services
+
+import com.github.vladogol.intellijkeymapexporter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
